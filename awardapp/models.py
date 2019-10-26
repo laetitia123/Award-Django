0b60b02d95762e799ed163823dd0073db2ef4c2a
laetitia123 <uwizelaetitia@gmail.com>
2019-10-26 17:49:29 +0200

    rate model added

--- awardapp/models.py
+++ awardapp/models.py
@@ -83,6 +83,6 @@ class Rates (models.Model):
     design= models.PositiveIntegerField(default=0 ,validators=[MaxValueValidator(10)])
     usability= models.PositiveIntegerField(default=0 ,validators=[MaxValueValidator(10)])
     content= models.PositiveIntegerField(default=0 ,validators=[MaxValueValidator(10)])
-#     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
 #     posted_by=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
 #     project=models.IntegerField(default=0)  
