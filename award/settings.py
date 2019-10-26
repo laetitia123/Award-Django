a2df0a6d12c0ac14a93c5a7c4ca738bec145af9a
laetitia123 <uwizelaetitia@gmail.com>
2019-10-26 20:01:46 +0200

    adeed

--- award/settings.py
+++ award/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
@@ -46,7 +47,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
