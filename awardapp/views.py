81faf85c1cac896c65cb350f7a689f84184f941d
laetitia123 <uwizelaetitia@gmail.com>
2019-10-26 18:03:36 +0200

    search added

--- awardapp/views.py
+++ awardapp/views.py
@@ -218,7 +218,7 @@ def search_results(request):
 
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
-        # searched_articles = Project.search_by_title(search_term)
+        searched_articles = Project.search_by_title(search_term)
 #         message = f"{search_term}"
 
 #         return render(request, 'search.html',{"message":message,"projectes": searched_articles})
